0725fadca791b5e514895b114581c95e1a6afe48
SaumyaBhushan <saumya@knoldus.com>
2023-05-06 16:14:49 +0530

    added aws creds var

--- variables.tf
+++ variables.tf
@@ -3,3 +3,16 @@ variable "instance_type" {
     default = "t3.micro"
     description = "Type of the ec2 instance created"
 }
+
+variable "access_key" {
+    type = string
+    default = ""
+    description = "Access key of AWS"
+}
+
+
+variable "secret_key" {
+    type = string
+    default = ""
+    description = "Secret key of aws"
+}
